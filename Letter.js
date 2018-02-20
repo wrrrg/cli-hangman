bbcc04b4fa721ac6551a501ce14b2986fbd7bd01
wrrrg <bill.gryta@gmail.com>
2018-02-20 16:27:23 -0600

    changed Letter.js functions to prototype

--- Letter.js
+++ Letter.js
@@ -3,29 +3,38 @@
 var Letter = function(char){
   this.char = char;
   this.guessed = false;
+
+};
   // check if the letter has been guessed - if not, display a "_", if it has, display the char as a string
-  Letter.prototype.displayLetter = function(guessed){
-    var display = '';
+  Letter.prototype.isGuessedCheck = function() {
+
+    var char = this.char.toString();
+
     if(!this.guessed){
-      display = '_';
+      return '_';
     } else {
-      display = char.toString();
+      return char
     }
-    console.log(display);
   };
-  //  checks input vs. the char - if it's correct (either lower or uppercase will count) we change guessed boolean to true and run the display function
-  this.checkLetter = function(input){
-    if(input === this.char.toUpperCase() || this.char.toLowerCase()){
-      this.guessed = true;
-      this.displayLetter();
+
+  //  checks input vs. the char. Saving this as a separate function for now.
+
+  Letter.prototype.checkLetter = function(input, letterObj){
+    if(input.toUpperCase() === letterObj.char.toUpperCase()){
+      return letterObj.guessed = true;
     } else {
       console.log("Not the right answer!");
+      
     };
   };
-};
 
-var testLetter = new Letter("w");
+var testLetter = new Letter("W");
+
+// testLetter.checkLetter("f");
 console.log(testLetter);
-
-testLetter.checkLetter('s');
+// console.log(this);
+console.log(testLetter.guessed);
+console.log(testLetter.isGuessedCheck());
+console.log(testLetter.checkLetter("f", testLetter));
+// console.log(this.guessed);
 module.exports = Letter;
